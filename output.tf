b723fb26b0058251e9e307a31eb141fcbc1c2425
salonibhatnagar <47357459+salonibhatnagar@users.noreply.github.com>
2022-07-10 13:12:39 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.test.kube_config.0.client_certificate
+  value     = azurerm_kubernetes_cluster.aks-cluster.kube_config.0.client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.test.kube_config_raw
+  value = azurerm_kubernetes_cluster.aks-cluster.kube_config_raw
 
   sensitive = true
 }
