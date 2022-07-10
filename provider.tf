7df5401a8ebf7a9215fb26120f8e84bf733a954b
salonibhatnagar <47357459+salonibhatnagar@users.noreply.github.com>
2022-07-10 13:14:56 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,6 +11,6 @@ provider "azurerm" {
  features {}   
  subscription_id = "b4fd38ca-930c-4e35-a642-d7f7075f428e"
   client_id       = "d7400eae-a48a-4710-9256-d5f16cc12437"
-  client_secret   = "EOzYwgqbkZVLtawjvfsa"
+  client_secret   = "EOzYwgqbkZVLtawjvfF5qYXk.f3EdZUrbS"
   tenant_id       = "961e4a55-c338-498d-a310-544a2b8c027e"       
 }
