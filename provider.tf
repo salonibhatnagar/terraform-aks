fa612830f625dba99933f2a3d16f3f253b97d2b1
salonibhatnagar <47357459+salonibhatnagar@users.noreply.github.com>
2022-07-10 12:59:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,3 +11,11 @@ terraform {
 provider "azurerm" {
   features {}
 }
+# Provider Block
+provider "azurerm" {
+ features {}   
+ subscription_id = "b4fd38ca-930c-4e35-a642-d7f7075f428e"
+  client_id       = "d7400eae-a48a-4710-9256-d5f16cc12437"
+  client_secret   = "EOzYwgqbkZVLtawjvfsa"
+  tenant_id       = "961e4a55-c338-498d-a310-544a2b8c027e"       
+}
